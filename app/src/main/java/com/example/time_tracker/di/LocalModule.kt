50bdee55bfd9b6028b1a770223748aff028e2a05
bybuss <kukuhaha729@gmail.com>
2024-09-08 18:11:38 +0300

    added a module providing DAOs

--- app/src/main/java/com/example/time_tracker/di/LocalModule.kt
+++ app/src/main/java/com/example/time_tracker/di/LocalModule.kt
@@ -3,6 +3,14 @@ package com.example.time_tracker.di
 import android.content.Context
 import androidx.room.Room
 import com.example.time_tracker.data.local.room.AppDatabase
+import com.example.time_tracker.data.local.room.group.GroupDao
+import com.example.time_tracker.data.local.room.organization.OrganizationDao
+import com.example.time_tracker.data.local.room.project.ProjectDao
+import com.example.time_tracker.data.local.room.role.RoleDao
+import com.example.time_tracker.data.local.room.task.TaskDao
+import com.example.time_tracker.data.local.room.user.UserDao
+import com.example.time_tracker.data.local.room.userOrg.UserOrgDao
+import com.example.time_tracker.data.local.room.userTask.UserTaskDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -23,4 +31,36 @@ object LocalModule {
             .fallbackToDestructiveMigration()
             .build()
     }
+
+    @Provides
+    @Singleton
+    fun provideGroupDao(appDatabase: AppDatabase): GroupDao = appDatabase.groupDao()
+
+    @Provides
+    @Singleton
+    fun provideOrganizationDao(appDatabase: AppDatabase): OrganizationDao = appDatabase.organizationDao()
+
+    @Provides
+    @Singleton
+    fun provideProjectDao(appDatabase: AppDatabase): ProjectDao = appDatabase.projectDao()
+
+    @Provides
+    @Singleton
+    fun provideRoleDao(appDatabase: AppDatabase): RoleDao = appDatabase.roleDao()
+
+    @Provides
+    @Singleton
+    fun provideTaskDao(appDatabase: AppDatabase): TaskDao = appDatabase.taskDao()
+
+    @Provides
+    @Singleton
+    fun provideUserDao(appDatabase: AppDatabase): UserDao = appDatabase.userDao()
+
+    @Provides
+    @Singleton
+    fun provideUserOrgDao(appDatabase: AppDatabase): UserOrgDao = appDatabase.userOrgDao()
+
+    @Provides
+    @Singleton
+    fun provideUserTaskDao(appDatabase: AppDatabase): UserTaskDao = appDatabase.userTaskDao()
 }
